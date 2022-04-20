c78e75963496bec69057b2456f3b1c01cc58b215
troymeeker <troymeeker1@gmail.com>
2022-04-20 12:04:02 -0700

    setup complete, first commit

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+ 
+
+  return (
+    <div >
+     <h1>Old North Acres</h1>
+     <div>Login Component</div>
+     <div>Signup Component</div>
+    </div>
+  );
+}
+
+export default App;
