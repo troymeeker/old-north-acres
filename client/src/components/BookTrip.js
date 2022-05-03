00700e9461228955b1f13b8ed92bba53894da242
troymeeker <troymeeker1@gmail.com>
2022-05-03 16:33:08 -0700

    click outside working

--- client/src/components/BookTrip.js
+++ client/src/components/BookTrip.js
@@ -1,58 +1,45 @@
-import React, {useState, useRef, useEffect} from "react";
+import React, {useState} from "react";
 import yurt from "../images/yurt.jpg"
 import cabin from "../images/cabin.jpg"
 import schoolBus from "../images/school-bus.jpg"
- import Popup from "./Popup";
-
-
-
+import OutsideClickHandler from "react-outside-click-handler/build/OutsideClickHandler";
 
 
 function BookTrip(){ 
-     const ref = useRef(); 
-
-     const [popup, setPopup] = useState(false); 
-
-    useEffect(() => {
-        const checkClickOutside = (e) => {
-            if (popup  && ref.current && !ref.current.contains(e.target)){
-              setPopup(false);
-            }
-        };
-
-        document.addEventListener('mousedown', checkClickOutside)
-
-        return () => {
-            document.removeEventListener('mousedown', checkClickOutside)
-        };
-    }, [popup]);
-   
-   
-
-  
+ 
+   const [popup, setPopup] = useState(false); 
+             
+   function handleClose(){
+    setPopup(false);
+   }
 
 
     return (
-         <div className="book_trip" >
+         <div className="book_trip"> 
             <h2>Schedule your trip!</h2>
-         
-            <p>Form or calendar here to select trip and display trips for current user</p>
-            <p>Click on one of our options to learn more! </p>
-            <div className="image-grid" ref={ref}> 
-                
-                    <img src={yurt} alt="yurt" className="yurt-photo" onClick={() => setPopup((popup) => !popup)}/>
-                    <img src={cabin} alt="cabin" className="yurt-photo" onClick={() => setPopup((popup) => !popup)}/>
-                    <img src={schoolBus} alt="yurt" className="yurt-photo" onClick={() => setPopup((popup) => !popup)}/>
-             
-                 <Popup trigger={popup} setPopup={setPopup}>
-                    <h3>Lodge #_ </h3>
-                    <p>This rustic lodging option sleeps _ and is great for_. </p>
-                    <p>Click HERE to schedule your visit to Lodge #_</p>
-                </Popup>   
-              
-             </div>
-
-           
+          
+            {/* <p>Form or calendar here to select trip and display trips for current user</p>  */}
+             <p>Click on one of our options to learn more! </p>
+  
+          
+              <OutsideClickHandler onOutsideClick={() => setPopup(false)}>  
+                 <div className="image-grid" > 
+                    <img src={yurt} alt="yurt" className="yurt-photo" onClick={() => setPopup(true)}/>
+                    <img src={cabin} alt="cabin" className="yurt-photo" onClick={() => setPopup(true)}/>
+                    <img src={schoolBus} alt="yurt" className="yurt-photo" onClick={() => setPopup(true)}/>
+                </div>
+               </OutsideClickHandler >
+               { popup && ( 
+                    <div className="popup" >
+                      <div className="popup-content">              
+                      
+                        <h3>Lodge #_ </h3>
+                         <button className="popup-button" onClick={handleClose}>&times;</button>
+                        <p>This rustic lodging option sleeps _ and is great for_. </p>
+                        <p>Click HERE to schedule your visit to Lodge #_</p>
+                      </div>
+                    </div>
+                )}                
          </div> 
     );
 };
--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 import emailjs from 'emailjs-com';
 
