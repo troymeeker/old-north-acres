5a2a596ad7510ec828dc2bf8f5789d1a328f77ff
troymeeker <troymeeker1@gmail.com>
2022-06-08 13:18:29 -0700

    back to browserrouter and changed location of public folder w index.html to deploy

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import App from './components/App';
 
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-     <HashRouter>
+     <BrowserRouter>
           <App/>
-     </HashRouter>
+     </BrowserRouter>
 );
 
 
