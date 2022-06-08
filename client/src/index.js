ac333e869ff0eab8b75c4293bacaef9073a95b72
troymeeker <troymeeker1@gmail.com>
2022-06-08 11:32:03 -0700

    hashrouter

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import App from './components/App';
 
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-     <BrowserRouter>
+     <HashRouter>
           <App/>
-     </BrowserRouter>
+     </HashRouter>
 );
 
 
