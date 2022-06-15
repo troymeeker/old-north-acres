72e107e91accad7c6f8ac11845ab3dfeabf6280b
troymeeker <troymeeker1@gmail.com>
2022-06-14 22:02:13 -0700

    downgraded to rails 6 and added  config.autoload = :classic to application.rb

--- config/application.rb
+++ config/application.rb
@@ -43,6 +43,6 @@ module OldNorthAcres
 
     # Use SameSite=Strict for all cookies to help protect against CSRF
     config.action_dispatch.cookies_same_site_protection = :strict
- 
+    config.autoload = :classic
   end
 end
