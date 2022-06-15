1ffe58e5136795b1ca6526a3db15551c5004e4be
troymeeker <troymeeker1@gmail.com>
2022-06-15 15:13:05 -0700

    test classic mode

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import App from './components/App';
 
